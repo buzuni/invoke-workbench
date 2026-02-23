3a6b06e319edd6ed58d7211f877dee553292a68c
buzuni <buzantine@gmail.com>
2026-02-23 23:41:23 +0100

    fix: add pip timeout config and URL fix to preBuild.bash

--- preBuild.bash
+++ preBuild.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
-# This file contains bash commands that will be executed at the beginning of the container build process,
-# before any system packages or programming language specific package have been installed.
-#
-# Note: This file may be removed if you don't need to use it
+# Pre-build script: set up pip config for longer timeout
+mkdir -p /home/workbench/.config/pip
+cat > /home/workbench/.config/pip/pip.conf << 'EOF'
+[global]
+timeout = 300
+retries = 5
+EOF
+
+# Fix requirements.txt if it has the broken URL (missing space)
+if [ -f /opt/project/build/requirements.txt ]; then
+    sed -i 's/--extra-index-urlhttps/--extra-index-url https/' /opt/project/build/requirements.txt
+fi
